8bbc15f785087bdebb4793c2b55df3618acd5650
Moin <anmmoinuddin@gmail.com>
2020-03-17 14:07:54 +0100

    Create main.c

---
+++ lab11/main.c
@@ -0,0 +1,43 @@
+// author: moin & Elvis
+
+#include <avr/io.h>
+#include "init.h"
+
+
+int main(void){
+
+
+	// Init START
+/*	DDRD &= ~(1 << DDD2); // set PD2 data direction to input
+	DDRD &= ~(1 << DDD3); // set PD3 data direction to input
+
+	PORTD |= (1 << PD2); // enable internal pullup for PD2
+	PORTD |= (1 << PD3); // enable internal pullup for PD3
+
+	DDRB |= (1 << DDB1); // set PB1 data direction to output */
+	//Init END
+			init();				// call the function from int.c 
+
+	while(1){
+		if (!((~PIND & (1 << PD2)) && (~PIND & (1 << PD3))))
+			PORTB |= (1 << PB1);  
+			
+		else
+			PORTB &= ~(1 << PB1);	
+		
+		if ((~PIND & (1 << PD2)) || (~PIND & (1 << PD3)))
+			PORTB |= (1 << PB2);
+		
+		else
+			PORTB &= ~(1 << PB2);
+			
+		if (!(~PIND & (1 << PD2)) != !(~PIND & (1 << PD3)))
+			PORTB |= (1 << PB3);
+			
+		else
+			PORTB &= ~(1 << PB3);
+			
+	}
+	
+	return 0;
+}
