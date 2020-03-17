cdffe3e2442e26860dea7c2802f78e0cdc3d100b
Moin <anmmoinuddin@gmail.com>
2020-03-17 14:11:21 +0100

    Create main.c

---
+++ lab11/Bonus/main.c
@@ -0,0 +1,35 @@
+// author: moin
+
+#include <avr/io.h>
+#include "init.h"
+#include "led.h"
+
+int main(void){
+
+
+	
+			init();				// call the function from int.c 
+
+	while(1){
+		if (!((~PIND & (1 << PD2)) && (~PIND & (1 << PD3))))
+
+			ledRed(1);
+		else
+
+			ledRed(0);
+		if ((~PIND & (1 << PD2)) || (~PIND & (1 << PD3)))
+
+			ledYellow(1);
+		else
+
+			ledYellow(0);
+		if (!(~PIND & (1 << PD2)) != !(~PIND & (1 << PD3)))
+
+			ledGreen(1);
+		else
+
+			ledGreen(0);
+	}
+	
+	return 0;
+}
