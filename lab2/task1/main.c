62634eade9ea490fcd6356229ac58898d0d4c4c1
Moin <anmmoinuddin@gmail.com>
2020-03-17 14:14:56 +0100

    Add files via upload

--- lab2/task1/main.c
+++ lab2/task1/main.c
@@ -1,1 +1,71 @@
+ /**
+	@author Moin and Elvis
+	 
+	@file main.c
+	@brief ENTER discription
+*/
 
+/******************************************************************************/
+/* INCLUDED FILES                                                             */
+/******************************************************************************/
+#include <avr/io.h>
+// newly added
+#include <util/delay.h>
+#include <stdio.h>
+#include "lcd.h"
+/******************************************************************************/
+#define F_CPU 8000000UL //CPU clock
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/**
+	@brief Init the microcontroller
+*/
+void init(void){
+	// Digital I/O init----------------------------------------------------
+	DDRB &= ~(1 << DDB0);		// PB0 as Input (key 1)
+	PORTB |= ( 1 << PB0);		// Pullup PB0
+
+	DDRB |=  (1 << DDB1);		// PB1 as Output (LED red)
+	DDRB |=  (1 << DDB2);		// PB1 as Output (LED yellow)
+	DDRB |=  (1 << DDB3);		// PB1 as Output (LED green)
+
+
+	
+
+}
+/******************************************************************************/
+
+
+/**
+	@brief Main function
+	@return only a dummy to avoid a compiler warning, not used
+*/
+int main(void){
+	
+	
+	
+	// Initialize the function
+	init();
+
+	// Loop forever
+	while (1){
+	
+		// Task 1 
+		//If buttio is pressed, the led is on, in other case off.
+		 
+		if ( (PINB & (1 << PB0))  )
+			PORTB &= ~(1 << PB1);
+			
+		else
+			PORTB |= (1 << PB1);
+		
+		
+			
+	
+	}
+	return 0;
+}
+/******************************************************************************/
